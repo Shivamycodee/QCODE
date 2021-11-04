2e7d958514b4375575bea2232340a23fdf866e83
Shivamycodee <91057727+Shivamycodee@users.noreply.github.com>
2021-11-04 09:31:42 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,90 @@
+
+const emailEl = document.getElementById("email-el")
+const fNameEl = document.getElementById("fname-el")
+const passEl = document.getElementById("set-pass-el")
+const signInEl = document.getElementById("sign-in-el")
+const signUpEl=document.getElementById("sign-up-el")
+const checkPass=document.getElementById("check-pass")
+const checkEmail=document.getElementById("check-email")
+
+var Userdata=[]
+
+signUpEl.addEventListener("click",function(){
+  Userdata.push({emaild:emailEl.value,passd:passEl.value})
+  // localStorage.setItem("Email",emailEl.value)
+  localStorage.setItem("userData",JSON.stringify(Userdata))
+  open("sign-in.html","_self")
+
+
+})
+
+function practice(){
+  open("https://www.w3schools.com/")
+}
+
+
+// function conform(){
+// var Emailset= localStorage.getItem("Email")
+// var checkmail =checkEmail.value
+//       if(Emailset==checkmail){
+//         open("website1.html")
+//       }
+//       else{
+//         alert("❌ Wrong Email id or password.")
+//       }
+//     }
+var n =0
+function conform(){
+  var dataArray = JSON.parse(localStorage.getItem("userData"));
+  for(var i=0;i<dataArray.length;i++){
+    var k=dataArray[i].emaild
+    var p=dataArray[i].passd
+   if(k == checkEmail.value && p== checkPass.value){
+      open("website1.html")
+      break;
+      }
+      else{
+        n++
+      }
+    }
+     if(n!=0){
+       alert("Wrong login id or password")
+     }
+    
+  }
+
+
+ function sendmail(){
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "shivamyadav35707@gmail.com",
+      Password: "@.3s9879",
+      To: "shivamyadav8758@gmail.com",
+      From: "shivamyadav35707@gmail.com",
+      Subject: "Sending Email using javascript",
+      Body: "Well that was easy!!",
+    })
+      .then(function (message) {
+        alert(`mail sent successfully. look ${fNameEl.value} 😒 we  have sent the conformation mail but google is blocking it because of some security issues  so i can't do anything about sorry`)
+      });
+  }
+
+  
+
+const togglePasswordButton = document.getElementById('toggle-password');
+
+togglePasswordButton.addEventListener('click', togglePassword);
+
+function togglePassword() {
+  if (passEl.type === 'password') {
+    passEl.type = 'text';
+  }else  {
+    passEl.type = 'password';
+  }
+}
+ 
+
+const Twitter=document.getElementById('twitter')
+
+
+
